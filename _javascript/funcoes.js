8740b4cfba3093cbc55619e9808e387b4dee55d2
gracibrea <claudiagracieli@gmail.com>
2021-11-27 17:37:25 -0300

    first commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,4 @@
+function mudaFoto (foto)
+{
+    document.getElementById("icone").src = foto; /*icone porque é o id da imagem principal <img id="icone" src="_imagens/glass-oculos-preto-peq.png" alt="oculos">*/
+}
